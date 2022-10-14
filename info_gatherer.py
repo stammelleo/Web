e0f5f1c76233901d01b4b3973b9e1912e77a24fd
seantammelleo <Spartanlp@hotmail.com>
2022-10-14 12:45:29 -0400

    converting list in carrot_text_filter back into string

--- info_gatherer.py
+++ info_gatherer.py
@@ -8,21 +8,31 @@ from link_gatherer import *
 
 def string_to_list_convert(text):
     # Converts given string to list and returns list
+
     temp_string = []
     for character in text:
         temp_string.append(character)
     return temp_string
 
 
+def list_to_string_convert(bucket):
+    r_string = ''
+    # iterate through given list adding to string
+    for item in bucket:
+        r_string += item
+    return r_string
+
+
+
 def easter_text_finder(page_text):
     final_string = ''
     result = [_.start() for _ in re.finditer(' Easter ', page_text)]
     for index in result:
-        final_string += page_text[index - 100: index + 100]
+        final_string += page_text[index - 300: index + 500]
         print('-' * 30)
     result = [_.start() for _ in re.finditer(' easter ', page_text)]
     for index in result:
-        final_string += page_text[index - 100: index + 100]
+        final_string += page_text[index - 300: index + 500]
         print('-' * 30)
     return final_string
 
@@ -31,39 +41,22 @@ def carrot_text_filter(text):
     index = 0
     # Create temporary list with chars to get rid of
     temp = string_to_list_convert(text)
-    print(temp)
-    temp2 = string_to_list_convert(text)
     start_range = 0
     # iterate through string removing non text
     for char in text:
         if char == '<' and start_range == 0:
             start_range = index
-        if char == '>' and start_range != 0:
-
-            print(len(temp))
-            print(text[start_range])
-            print(text[index])
-            print(f'The start range is {start_range} and the end range is {index}')
-
+        elif char == '>' and start_range != 0:
+            # Deleting <...> from strings
             del (temp[start_range: index + 1])
 
             # Recalculating index based on new length of set
             index = index - ((index + 1) - start_range)
-            print(len(temp))
-
             start_range = 0
         index += 1
 
-    # temp 2 should be bigger than temp
-    print (len(temp2))
-    print(temp2)
-    print(temp)
-    print(len(temp))
-    # iterate through what is left of string list and turn back into string
-    # print(f'the text is {temp}')
-    # text = ''
-    # text.join(str(s) for s in temp)
-    # print (f'the text is now {text}')
+    return list_to_string_convert(temp)
+
 # class InfoGatherer:
 
 # Gather string surrounding word Easter
